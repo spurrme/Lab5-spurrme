b791b34ff1c1f8f47596b14e463edd324944c0c0
spurrme <spurrme@rose-hulman.edu>
2012-03-22 15:01:34 -0400

    Set up the basic test of the complier

---
+++ Prime Factors/src/PrimFactorsTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author spurrme.
+ *         Created Mar 22, 2012.
+ */
+public class PrimFactorsTest {
+	@Test
+	public void testComplier(){
+		assertTrue("Does this work?", true);
+	}
+}
---
+++ Prime Factors/src/Prime_Factors.java
@@ -0,0 +1,10 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author spurrme.
+ *         Created Mar 22, 2012.
+ */
+public class Prime_Factors {
+
+}
