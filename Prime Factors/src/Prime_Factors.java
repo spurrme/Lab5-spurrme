4c7c9a28291a52d81ba27f4086384e61c4693a8f
spurrme <spurrme@rose-hulman.edu>
2012-03-22 15:18:39 -0400

    passed the test case for 4

--- Prime Factors/src/Prime_Factors.java
+++ Prime Factors/src/Prime_Factors.java
@@ -11,6 +11,10 @@ public class Prime_Factors {
 
 	public static ArrayList<Integer> getFactors(int i) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
+		if(i%2 == 0){
+			ret.add(2);
+			i/=2;
+		}
 		if(i > 1){
 			ret.add(i);
 		}
