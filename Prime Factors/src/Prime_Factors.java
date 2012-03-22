117e37e2267445604b3062144da530f8c47a0025
spurrme <spurrme@rose-hulman.edu>
2012-03-22 16:00:48 -0400

    passed the test for generate 5

--- Prime Factors/src/Prime_Factors.java
+++ Prime Factors/src/Prime_Factors.java
@@ -24,11 +24,26 @@ public class Prime_Factors {
 	public static ArrayList<Integer> generatePrime(int i) {
 		ArrayList<Integer> ret =  new ArrayList<Integer>();
 		if(i > 2){
-			for (int j = 2; j < i; j++) {
-				ret.add(j);
+			ret.add(2);
+			for (int j = 3; j < i; j++) {
+				if(testIsPrime(j)){
+					ret.add(j);
+				}
 			}
 		}
 		return ret;
+	}
+
+	private static boolean testIsPrime(int j) {
+		for (int i = 2; i < j; i++) {
+			if(j%i == 0){
+				return false;
+			}
+			else{
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
